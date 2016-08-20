e5f68f9320f0f0e7ce0caa4d88bd49aafc2e2b91
Hossain Khan <amardeshbd@gmail.com>
2016-08-20 09:29:25 -0400

    [FIXED] Validation issue. Moved the variable echo inside test.

--- api-spec_validation_test.sh
+++ api-spec_validation_test.sh
@@ -5,22 +5,21 @@ setUp()
   originalPath=$PATH
   PATH=$PWD:$PATH
   
-
-  echo "Validating ENV Variables: TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR, BRANCH=$BRANCH"
-  echo "OpenAPI Specification File=$OPEN_API_SPEC_FILE"
-  echo ""
   echo "Executing tests... ^_^"
 }
 
 testOpenApiSpecValidity() {
-	validationOutput=$(curl http://online.swagger.io/validator/debug?url=http://petstore.swagger.io/v2/swagger.json)
-	echo "Testing swagger validation - current output is: $validationOutput"
-	assertEquals "Validation failed" "{}" "$validationOutput"
+  echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+  echo "Validating ENV Variables: TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR, BRANCH=$BRANCH"
+  echo "OpenAPI Specification File=$OPEN_API_SPEC_FILE"
+  echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+
+    validationOutput=$(curl http://online.swagger.io/validator/debug?url=http://petstore.swagger.io/v2/swagger.json)
+    echo "Testing swagger validation - current output is: $validationOutput"
+    echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+    assertEquals "Validation failed - service unavailable or error found." "{}" $validationOutput
 }
 
-testFailCase() {
-  assertEquals 1 99
-}
 
 # Execute shunit2 to run the tests (downloaded via `.travis.yaml`)
 . shunit2-2.1.6/src/shunit2
