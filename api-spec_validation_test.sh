f4f8cc9179b3ade951207fc3e5e4bf674e6830d1
Hossain Khan <amardeshbd@gmail.com>
2016-08-20 08:51:49 -0400

    [UPDATE] Curl output variable was not available in bash. Moved inside test method.

--- api-spec_validation_test.sh
+++ api-spec_validation_test.sh
@@ -9,9 +9,10 @@ setUp()
 }
 
 testEquality() {
+	validationOutput=$(curl http://online.swagger.io/validator/debug?url=http://petstore.swagger.io/v2/swagger.json)
 	echo "Testing swagger validation - current output is: $validationOutput"
 	assertEquals $validationOutput "{}"
 }
 
-# Execute shunit2 to run the tests
+# Execute shunit2 to run the tests (downloaded via `.travis.yaml`)
 . shunit2-2.1.6/src/shunit2
