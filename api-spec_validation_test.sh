e0cc5efaca7543bab042b8ddfdb355cbba6d691f
Hossain Khan <amardeshbd@gmail.com>
2016-08-20 10:10:43 -0400

    [FIXED] Finally worked around following issue.
    # Instead of comparing, string output, I had to calculate string size because if service
    # gives error response, the unit test validation fails. See sample below
    #
    # Sample RESPONSE: {"schemaValidationMessages":[{"level":"error","message":"Can't read from file http://bla.bla.json"}]}
    #
    # shunit2:ERROR assertEquals() requires two or three arguments; 7 given
    # shunit2:ERROR 1: Validation failed {} : {"schemaValidationMessages":[{"level":"error","message":"Can't 4: read

--- api-spec_validation_test.sh
+++ api-spec_validation_test.sh
@@ -4,22 +4,48 @@ setUp()
 {
   originalPath=$PATH
   PATH=$PWD:$PATH
+
+  # Setup local variables to dynamically generate the API spec URL from pull request.
+  githubRawResourceBaseUrl=https://raw.githubusercontent.com
+  githubUsername=amardeshbd
+  githubProjectId=medium-api-specification
+  openApiSpecFileName=medium-api-specification.yaml
+
   
   echo "Executing tests... ^_^"
 }
 
+
+# Tests the swagger using online service
+#
+# DEV NOTE:
+# ----------------
+# Instead of comparing, string output, I had to calculate string size because if service
+# gives error response, the unit test validation fails. See sample below
+#
+# Sample RESPONSE: {"schemaValidationMessages":[{"level":"error","message":"Can't read from file http://bla.bla.json"}]}
+#
+# shunit2:ERROR assertEquals() requires two or three arguments; 7 given
+# shunit2:ERROR 1: Validation failed {} : {"schemaValidationMessages":[{"level":"error","message":"Can't 4: read
+#
 testOpenApiSpecValidity() {
   echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
   echo "Validating ENV Variables: TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR, BRANCH=$BRANCH"
   echo "OpenAPI Specification File=$OPEN_API_SPEC_FILE"
   echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
-
-    validationOutput=$(curl http://online.swagger.io/validator/debug?url=http://petstore.swagger.io/v2/swagger.json)
+    
+    expectedOutput="{}"
+    expectedOutputSize=${#expectedOutput} 
+    
+    specUrl="$githubRawResourceBaseUrl/$githubUsername/$githubProjectId/$BRANCH/$openApiSpecFileName"
+    validationOutput=$(curl $specUrl)
+    validationOutputSize=${#validationOutput}
     echo "Testing swagger validation - current output is: $validationOutput"
+    echo "Expected valid size: $expectedOutputSize, current output: $validationOutputSize"
     echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
-    assertEquals "Validation failed - service unavailable or error found." "{}" $validationOutput
+    
+    assertEquals "Validation failed - service unavailable or error found." $expectedOutputSize $validationOutputSize
 }
-
 
 # Execute shunit2 to run the tests (downloaded via `.travis.yaml`)
 . shunit2-2.1.6/src/shunit2
